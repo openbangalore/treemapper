67092209660f2729c602b4a4865e46566aa410aa
Rushabh Mehta <rmehta@gmail.com>
2013-08-12 18:38:36 +0530

    build gh pages in make html

--- build.py
+++ build.py
@@ -18,6 +18,8 @@ def make_html():
 			html = jenv.get_template(fname).render(args)
 			with open(os.path.join("public", fname), "w") as outfile:
 				outfile.write(html)
+				
+	make_gh_pages()
 
 def make_gh_pages():
 	if not os.path.exists("gh_pages"):
@@ -27,4 +29,3 @@ def make_gh_pages():
 if __name__=="__main__":
 	database.connect()
 	make_html()
-	make_gh_pages()
